c4826ae4c851f92000982bfa3750ba905d0d8e4c
ZalanMede <126560986+ZalanMede@users.noreply.github.com>
2023-06-04 22:11:31 +0200

    Véglegesített mindkét weboldal
    Mindkét weboldal készen áll a publikációra és mobil eszközökön is működnek. Hozzáadva: interaktív menü, képek, külön forrás weboldal linkkel az alján, információ, pörgő lemez megoldva

--- halhatterblend.js
+++ halhatterblend.js
@@ -2,7 +2,7 @@ const [red, green, blue] = [65,105,225]
 const section2 = document.querySelector('.section2')
 
 window.addEventListener('scroll', () => {
-  const y = 1 + (window.scrollY || window.pageYOffset) / 1500
+  const y = 1 + (window.scrollY || window.pageYOffset) / 2500
   const [r, g, b] = [red/y, green/y, blue/y].map(Math.round)
   section2.style.backgroundColor = `rgb(${r}, ${g}, ${b})`
 })
